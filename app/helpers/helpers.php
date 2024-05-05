47cc98a61dd16ebae3dd6cde7cbfd22c44481d53
amirrezaraadi <amirrezaraadi@yahoo.com>
2024-05-05 13:34:02 +0330

    add helper in project , using to composer.json

---
+++ app/helpers/helpers.php
@@ -0,0 +1,14 @@
+<?php
+
+
+if ( ! function_exists('client_ip')) {
+    function client_ip($withData = false)
+    {
+        $ip = $_SERVER['REMOTE_ADDR'] . '-' . md5($_SERVER['HTTP_USER_AGENT']);
+        if($withData) {
+            $ip .= '-' . now()->toDateString();
+        }
+
+        return $ip;
+    }
+}
