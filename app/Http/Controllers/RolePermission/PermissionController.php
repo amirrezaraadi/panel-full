14e475b1bb46fc60ab447e13abf8cd25c56b09d1
amirrezaraadi <amirrezaraadi@yahoo.com>
2024-06-06 11:51:47 +0330

    finish  PermissionRequest

--- app/Http/Controllers/RolePermission/PermissionController.php
+++ app/Http/Controllers/RolePermission/PermissionController.php
@@ -3,19 +3,22 @@
 namespace App\Http\Controllers\RolePermission;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\RolePermission\PermissionRequest;
 use App\Models\RolePermission\Permission;
+use App\Repository\RolePermission\roleRepo;
 use Illuminate\Http\Request;
 
 class PermissionController extends Controller
 {
+    public function __construct(public roleRepo $roleRepo){}
 
     public function index()
     {
-        //
+        return $this->roleRepo->index();
     }
 
 
-    public function store(Request $request)
+    public function store(PermissionRequest $request)
     {
         //
     }
